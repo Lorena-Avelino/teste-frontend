646b18cd0b8f06bbe9e285b41481316bf080fb95
Lorena Avelino <lorenaavelino28@gmail.com>
2023-07-14 12:04:41 -0300

    first commit

---
+++ src/components/breadcrumbs/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+import combinedShape from '../../assets/CombinedShape.svg'
+
+const BreadCrumbs = () => {
+    return (
+        <div className='breadCrumbs'>
+            <img src={combinedShape} className="combinedShape" alt="Combined Shape" />
+            <div className='retangle'></div>
+            <h2 className='menu'>Análise Geral <div className='barra'>/</div> <div className='principal'>Principal</div></h2>
+        </div>
+    )
+}
+
+export default BreadCrumbs;
